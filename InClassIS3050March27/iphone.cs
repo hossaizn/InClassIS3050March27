b16af2bf16031dcf1887763bd9bbe9c7012ed114
Leah T slassie <tslassll@mail.uc.edu>
2025-03-27 12:01:07 -0400

    Print Name for Person 2.

--- InClassIS3050March27/iphone.cs
+++ InClassIS3050March27/iphone.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+File Name: Program.cs
+
+*Name: Zulqarnayan Hossain, Liya T Slassie
+*email:  hossaizn@mail.uc.edu, tslassll@mail.uc.edu
+*Assignment Number: In Class Assignment March 27, 2025
+*Due Date: March 27, 2025
+*Course #/Section:   IS 3050 Sectiion 001
+* Semester / Year:   Spring 2025
+*Brief Description of the assignment:  We create a repository and perform pull requests to it as a collaborative effort.
+
+*Brief Description of what this module does.  We work on our understanding of using github as a collaborative coding tool.
+*Citations: N/A
+*Anything else that's relevant: N/A
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
